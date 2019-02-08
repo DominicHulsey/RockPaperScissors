3351c52f8fed43b6e84a7289b03ad26d9c9e5ef4
Dominic Hulsey <dominic.hulsey.dh@gmail.com>
2019-02-07 20:09:45 -0700

    fixed bug where computer answer doesn't reset

--- app/app.js
+++ app/app.js
@@ -5,63 +5,60 @@ let computerOptions = {
   3: 'Scissors'
 }
 
+let playerScore = 0;
+let computerScore = 0;
+
 function ComputerChoice() {
-  return Math.floor(Math.random() * 3) + 1;
-}
-
-var computerAnswer = computerOptions[ComputerChoice()];
-console.log(computerAnswer);
-var playerScore = 0;
-var computerScore = 0;
-function winner() {
-  let alertBox = `<div class="col-12" id="result"><div class='alert alert-success'>
-    <h1>You Win! </h1><p>the computer chose ${computerAnswer} smh...</p><br> <button class="btn btn-primary" onclick="replay()">Play Again!</button>
-</div >`
-  document.getElementById('alert').innerHTML = alertBox;
-  return playerScore++;
-}
-function loser() {
-  let alertBox = `<div class="col-12" id="result"><div class='alert alert-danger'>
-    <h1>You lose!</h1> <p>Sorry, the computer chose ${computerAnswer}...</p><br> <button class="btn btn-primary" onclick="replay()">Play Again!</button>
-</div>`
-  document.getElementById('alert').innerHTML = alertBox;
-  computerScore++
-}
-function tie() {
-  let alertBox = `<div class="col-12" id="result"><div class='alert alert-info'>
-    <h1>Tie!</h1><br><button class="btn btn-primary" onclick="replay()">Play Again!</button>
-</div>`
-  document.getElementById('alert').innerHTML = alertBox;
-}
-
-function dontClick() {
-  let alertBox = `<div class="col-12" id="result"><div class='alert alert-info'>
-    <h1>Don't click that.</h1><p>You lose a point</p><br><button class="btn btn-primary" onclick="replay()">Back to game</button>
-</div>`
-  document.getElementById('alert').innerHTML = alertBox;
-  playerScore--;
-}
-
-function PlayerChoice(choice) {
-  if (computerAnswer == choice) {
-    tie();
-  }
-  else if (computerAnswer == 'Rock' && choice == 'Paper') {
-    winner();
-  } else if (computerAnswer == 'Paper' && choice == 'Scissors') {
-    winner();
-  } else if (computerAnswer == 'Scissors' && choice == 'Rock') {
-    winner();
-  }
-  else { loser(); }
+  let output = Math.floor(Math.random() * 3) + 1;
+  return output;
 }
 
 function replay() {
   document.getElementById('alert').innerHTML = '';
-  ComputerChoice();
   document.getElementById('player').innerHTML = `<u>Player Score:</u> <br> ${playerScore}`;
   document.getElementById('computer').innerHTML = `<u>Computer Score:</u> <br> ${computerScore}`;
 }
+
+
+function PlayerChoice(choice) {
+  let computerAnswer = computerOptions[ComputerChoice()];
+  if (computerAnswer == choice) {
+    let alertBox = `<div class="col-12" id="result"><div class='alert alert-info'>
+    <h1>Tie!</h1><br><button class="btn btn-primary" onclick="replay()">Play Again!</button>
+</div>`
+    document.getElementById('alert').innerHTML = alertBox;
+  }
+  else if (computerAnswer == 'Rock' && choice == 'Paper') {
+    let alertBox = `<div class="col-12" id="result"><div class='alert alert-success'>
+    <h1>You Win! </h1><p>the computer chose ${computerAnswer} smh...</p><br> <button class="btn btn-primary" onclick="replay()">Play Again!</button>
+</div >`
+    document.getElementById('alert').innerHTML = alertBox;
+    return playerScore++;
+  } else if (computerAnswer == 'Paper' && choice == 'Scissors') {
+    let alertBox = `<div class="col-12" id="result"><div class='alert alert-success'>
+    <h1>You Win! </h1><p>the computer chose ${computerAnswer} smh...</p><br> <button class="btn btn-primary" onclick="replay()">Play Again!</button>
+</div >`
+    document.getElementById('alert').innerHTML = alertBox;
+    return playerScore++;
+  } else if (computerAnswer == 'Scissors' && choice == 'Rock') {
+    let alertBox = `<div class="col-12" id="result"><div class='alert alert-success'>
+    <h1>You Win! </h1><p>the computer chose ${computerAnswer} smh...</p><br> <button class="btn btn-primary" onclick="replay()">Play Again!</button>
+</div >`
+    document.getElementById('alert').innerHTML = alertBox;
+    return playerScore++;
+  }
+  else {
+    let alertBox = `<div class="col-12" id="result"><div class='alert alert-danger'>
+    <h1>You lose!</h1> <p>Sorry, the computer chose ${computerAnswer}...</p><br> <button class="btn btn-primary" onclick="replay()">Play Again!</button>
+</div>`
+    document.getElementById('alert').innerHTML = alertBox;
+    computerScore++
+  }
+
+}
+
+
+
 
 
 
